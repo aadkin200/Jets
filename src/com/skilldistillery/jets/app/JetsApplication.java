4d990ef96beba5b4bead5d2c1bae99d0b6848712
Alex Adkins <alex.t.adkins@gmail.com>
2021-05-28 17:23:23 -0400

    init commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.jets.app;
+
+import java.util.Scanner;
+
+import com.skilldistillery.jets.entity.AirField;
+
+public class JetsApplication {
+	private AirField airField;
+	private Scanner input;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/skilldistillery/jets/entity/AirField.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.jets.entity;
+
+import java.util.ArrayList;
+
+public class AirField {
+	private ArrayList<Jet> jets;
+}
